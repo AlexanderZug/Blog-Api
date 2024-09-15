5673b3ac7b05718d0dc05b26b64b56185516968e
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2024-09-15 15:52:09 +0200

    Fix ruff errors

--- app/account/models.py
+++ app/account/models.py
@@ -1,38 +1,39 @@
 from __future__ import annotations
 
+import contextlib
 import datetime
 import uuid
-from typing import Union
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Manager
 from django.utils import timezone
 
-try:
+with contextlib.suppress(ImportError):
     from blog.models import Blog, ReadStatus, Subscription
-except ImportError:
-    pass
 
 
 class User(AbstractUser):
     first_name = models.CharField("Имя", max_length=150)
     last_name = models.CharField("Фамилия", max_length=150)
     username = models.CharField(
-        "Логин", max_length=150, default=uuid.uuid4, unique=True
+        "Логин",
+        max_length=150,
+        default=uuid.uuid4,
+        unique=True,
     )
     email = models.EmailField("Email", unique=True)
-    blog: Union[Blog, Manager]
-    subscriptions: Union[Subscription, Manager]
-    verify_tokens: Union[VerifyToken, Manager]
-    read_statuses: Union[ReadStatus, Manager]
-
-    def __str__(self) -> str:
-        return str(self.first_name)
+    blog: Blog | Manager
+    subscriptions: Subscription | Manager
+    verify_tokens: VerifyToken | Manager
+    read_statuses: ReadStatus | Manager
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+    def __str__(self) -> str:
+        return str(self.first_name)
 
 
 class VerifyToken(models.Model):
@@ -46,26 +47,24 @@ class VerifyToken(models.Model):
         "имя",
         max_length=70,
         blank=True,
-        null=True,
     )
     last_name = models.CharField(
         "фамилия",
         max_length=80,
         blank=True,
-        null=True,
     )
     email = models.EmailField("Email", blank=True, null=True, unique=True)
     token = models.PositiveIntegerField("Токен")
     uuid = models.UUIDField("Uuid", default=uuid.uuid4)
     activation_date = models.DateTimeField("Дата деактивации", auto_now_add=True)
 
-    @property
-    def email_expired(self) -> bool:
-        return self.activation_date + datetime.timedelta(hours=1) < timezone.now()
+    class Meta:
+        verbose_name = "Токен"
+        verbose_name_plural = "Токены"
 
     def __str__(self) -> str:
         return str(self.email)
 
-    class Meta:
-        verbose_name = "Токен"
-        verbose_name_plural = "Токены"
+    @property
+    def email_expired(self) -> bool:
+        return self.activation_date + datetime.timedelta(hours=1) < timezone.now()
